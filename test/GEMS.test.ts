fee19f63f3bb7c7f2e85c2da38e20898b2e20d5f
Gunnar Mein <gunnar@together.science>
2023-07-27 13:35:42 -0700

    node fetch

--- test/GEMS.test.ts
+++ test/GEMS.test.ts
@@ -1,5 +1,9 @@
 import { GEMS } from '../src/index'
 
-test('init', () => {
-  expect(0).toBe(0)
+test('init', async () => {
+    const apiKey = "i2slulN)U%7xvMoVACLSEYogOekNQoWE";
+    const appId = "37675ac8-c0c0-42e9-8291-0f9529df5d47";
+    let response = await GEMS.init({apiKey:apiKey, appId:appId});
+    expect(response.userId?.length).toBeGreaterThan(5);
+    expect(response.token?.length).toBeGreaterThan(10);
 })
